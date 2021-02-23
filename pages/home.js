919eb6907abc8c8f804d1ac25136f615f36fd4cf
Steven Passynkov <passynkovsteven@gmail.com>
2021-02-22 20:47:59 -0500

    Added changes to the [graf].js

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import grafdata from "../graf_data.json";
 import Button from "react-bootstrap/Button";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Card from "react-bootstrap/Card";
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
 
 export default function HomePage() {
   return (
@@ -18,24 +20,28 @@ export default function HomePage() {
         <Breadcrumb.Item active>Home</Breadcrumb.Item>
       </Breadcrumb>
 
-      {grafdata.map((grafdata) => {
-        const { description, id, src } = grafdata;
-        return (
-          <div className={styles.main}>
-            <p id={id}>
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" scr="" />
-                <Card.Body>
-                  <Card.Text>{description}</Card.Text>
-                  <Link href={src}>
-                    <Button variant="outline-dark">Go to {description}</Button>
-                  </Link>
-                </Card.Body>
-              </Card>
-            </p>
-          </div>
-        );
-      })}
+      <Container>
+        <Row>
+          {grafdata.map((grafdata) => {
+            const { description, id, src } = grafdata;
+            return (
+              <Col key={id}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" scr="" />
+                  <Card.Body>
+                    <Card.Text>{description}</Card.Text>
+                    <Link href={src}>
+                      <Button variant="outline-dark">
+                        Go to {description}
+                      </Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
